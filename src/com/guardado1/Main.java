33aa5d2b7296ad9dfd2166500e010ec92b177845
00099216 <00099216@uca.edu.sv>
2018-06-05 13:53:15 -0600

    Se creó lo visto en clase

---
+++ src/com/guardado1/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guardado1;
+
+import com.guardado.gui.Gui;
+
+/**
+ *
+ * @author UCA
+ */
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run(){
+                new Gui().setVisible(true);
+        }
+        });
+        
+        new Gui().setVisible(true);
+    }
+    
+}
